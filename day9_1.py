471ceb6380380594508a51acf98badbd26ca5afd
GundarsM <127497152+GundarsM@users.noreply.github.com>
2023-12-17 20:25:25 +0200

    progress on9

--- day9_1.py
+++ day9_1.py
@@ -1,4 +1,6 @@
-file = open("input_day9.txt")
+import sys
+
+file = open("input_day9_t.txt")
 all_history = []
 
 for line in file:
@@ -18,12 +20,12 @@ for history in all_history:
     next_history =[]
     sum = 0
     refine = 1
-    extended_history.append(history)
+    extended_history.append([history[-1]])
     while refine:
         for i in range(len(history)-1):
-            print(i)
+            # print(i)
             # print(history)
-            dif = history[i+1]-history[i]
+            dif = abs(history[i+1]-history[i])
             sum+=dif
             next_history.append(dif)
             # if (len(next_history)==(len(history)-1)):
@@ -47,16 +49,35 @@ for history in all_history:
         # print("CCC")
         # print("H:", history)
         # print("NH:", history)
-        extended_history.append(history)
-        
-    print("EH: ",extended_history)
-    for j in range(len(extended_history)-1,-1,-1):
-        v1 = extended_history[j][len(extended_history[j])-1]
-        v2 = extended_history[j-1][len(extended_history[j-1])-1]
-        v3 = v1+v2
-        extended_history[j-1].append(v3)
-    print(v3)
+
+        if len(history)==0:
+            # print("empty")
+            extended_history.append([0])
+        else:
+            extended_history.append([history[-1]])
+        # value = input("Please enter a string:\n")
+    # print("EH: ",extended_history)
+    for j in range(len(extended_history)-1,0,-1):
+        v1 = extended_history[j][-1]
+        v2 = extended_history[j-1][-1]
+        if v2<0:
+            v3 = v2-v1
+        else:
+            v3 = v1+v2
+        # print("v3-1",v3,v2,v1)
+        if j == len(extended_history)-1:
+            # print("aaa")
+            extended_history[j].append(v1)
+            extended_history[j-1].append(v3)
+        else:
+            # print("bbb")
+            extended_history[j-1].append(v3)
+        # print("EHN_:",extended_history)
+    # print("v3",v3)
+
     summ += v3
     print("EHN:",extended_history)
-
+    # value = input("Please enter a string:\n")
 print("sum:",summ)
+if summ>100:
+    sys.exit(1)
